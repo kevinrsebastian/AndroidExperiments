0632b5023843fc24ef57bc6f87d58eb16598b1c8
Kevin Sebastian <kevinrsebastian@gmail.com>
2023-04-03 09:15:59 +0800

    Add and implement the AndroidX Splash Screen library

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 An android app for experimenting features and libraries. Also serves as a demo and quick code reference for
 replicating these features.
+
+---
+
+### Features and Libraries
+
+**[Splash Screen](https://developer.android.com/develop/ui/views/launch/splash-screen)**\
+This app implements a splash screen shown when the app is started. The
+[compat library](https://developer.android.com/develop/ui/views/launch/splash-screen/migrate) approach was used to
+support phones running android API 30 and below.
--- app/src/main/java/com/kevinrsebastian/androidex/MainActivity.kt
+++ app/src/main/java/com/kevinrsebastian/androidex/MainActivity.kt
@@ -2,9 +2,17 @@ package com.kevinrsebastian.androidex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        // Returns a SplashScreen object which can be used to customize the animation or duration.
+        installSplashScreen().apply {
+            // Checks continuously and if the condition is true, keep showing splash screen.
+            // this.setKeepOnScreenCondition { true }
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
