7e69cde75f1efd0f6355b6573152c272ad90077f
Kevin Sebastian <kevinrsebastian@gmail.com>
2023-04-17 10:21:28 +0800

    Display DirectoryActvity list with dummy data
    Add a RecyclerView to DirectoryActvity's layout and populate the list
    with dummy texts.

--- app/src/main/java/com/kevinrsebastian/androidex/directory/DirectoryActivity.kt
+++ app/src/main/java/com/kevinrsebastian/androidex/directory/DirectoryActivity.kt
@@ -3,6 +3,7 @@ package com.kevinrsebastian.androidex.directory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kevinrsebastian.androidex.R
 import com.kevinrsebastian.androidex.databinding.ActivityDirectoryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,10 +11,39 @@ import dagger.hilt.android.AndroidEntryPoint
 /** An activity that displays and redirects to the other demo activities. */
 @AndroidEntryPoint
 class DirectoryActivity : AppCompatActivity() {
+    companion object {
+        /** Temporary variable to populate the directory list. Public due to testing. */
+        var directoryItems = listOf(
+            "Lorem Ipsum Sit Amet",
+            "Consectetur Adipiscing Elit",
+            "Donec Ultricies Tincidunt Mi",
+            "Nulla Tristique Metus eu Tortori Lobortis in Posuere Lorem",
+            "Cras Semper",
+            "Fi Mili Sans",
+            "Uis Tincidunt Tempor",
+            "Vestibulum Felis",
+            "Quisque d Rhoncus Nisi",
+            "Donec Laoreet",
+            "Iiguadalia",
+            "Curabitur Nisi Augue",
+            "Vivamus",
+            "Mauris Imperdiet",
+            "Nulla Tristique Metus"
+        )
+    }
+
     private lateinit var binding: ActivityDirectoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpBinding()
+    }
+
+    private fun setUpBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_directory)
+
+        // Directory List
+        binding.directoryList.layoutManager = LinearLayoutManager(this)
+        binding.directoryList.adapter = DirectoryListAdapter(directoryItems)
     }
 }
